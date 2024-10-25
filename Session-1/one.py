3a288352cfd35c5b9817e392329e1fb7a4e61cd2
ZoltanSzabo <zoltan.szabo@everseen.com>
2024-10-25 10:50:47 +0300

    test

---
+++ Session-1/one.py
@@ -0,0 +1,15 @@
+def hello(description):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            print(f"Hello {description.format(*args)}")
+            return func(*args, **kwargs)
+        return wrapper
+    return decorator
+
+
+@hello("{}")
+def function_one(string):
+    pass
+
+
+function_one("World")
